fc9b8db139c181bb1f661e4da3babfcb87c8ba5f
Toby Glaze <toby.r.glaze@gmail.com>
2020-05-04 12:30:33 -0700

    layout setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div id="wrapper">
+        <h1 id="title">Pomodoro Clock</h1>
+
+        <div id="break-label" class="section-label">
+          Break Length
+        </div>
+
+        <div id="break-buttons">
+          <button>Down</button>
+          <label for="break">21</label>
+          <button>Up</button>
+        </div>
+
+        <div id="session-label" class="section-label">
+          Session Length
+        </div>
+
+        <div id="session-buttons">
+          <button>Down</button>
+          <label for="session">20</label>
+          <button>Up</button>
+        </div>
+
+        <div id="session" class="section-label">
+          session<br></br>
+          clock
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
